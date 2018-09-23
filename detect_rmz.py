d1e02543ba3d1f42d1f6961c3ba44455a053cc41
unknown <vishalraghav101993@gmail.com>
2018-09-23 03:46:15 -0400

    Performed threshold by otsu's operator

--- detect_rmz.py
+++ detect_rmz.py
@@ -14,15 +14,17 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (3,3), 0)
 blackchat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rectKernel)
 
-gradX = cv2.Sobel(blackhar, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
+gradX = cv2.Sobel(blackchat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
 gradX = np.absolute(gradX)
 (minVal, maxVal) = (np.min(gradX), np.max(gradX))
 gradX = (255 * ((gradX - minVal) / (maxVal - minVal))).astype("uint8")
 
+gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
+thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+                       
 
 
-
-plt.imshow(blackchat, cmap=plt.cm.gray)
+plt.imshow(thresh, cmap=plt.cm.gray)
 plt.axis('off')
 plt.show()
 	

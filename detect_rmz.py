51c5bbcc109472057f15329389ca3ddeaed841f8
unknown <vishalraghav101993@gmail.com>
2018-09-23 04:10:43 -0400

    Using contours, detected machine readbale zones in passport images

--- detect_rmz.py
+++ detect_rmz.py
@@ -25,10 +25,26 @@ thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
 thresh = cv2.erode(thresh, None, iterations=4)
 
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+cnts = sorted(cnts,key=cv2.contourArea, reverse=True)
+for c in cnts:
+    (x,y,w,h) = cv2.boundingRect(c)
+    ar = w / float(h)
+    crWidth = w / float(gray.shape[1])
+    if ar > 5 and crWidth > 0.75:
+        pX = int((x + w) * 0.03)
+        pY = int((y + h) * 0.03)
+        (x, y) = (x - pX, y - pY)
+        (w, h) = (w + (pX * 2), h + (pY * 2))
+        roi = image[y:y + h, x: x + w].copy()
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        break
+        
+        
 
 
 
-plt.imshow(thresh, cmap=plt.cm.gray)
-plt.axis('off')
-plt.show()
+cv2.imshow("Image", image)
+cv2.imshow("ROI", roi)
+cv2.waitKey(0)
 	
